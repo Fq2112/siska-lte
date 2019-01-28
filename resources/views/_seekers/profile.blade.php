467c2c3b50a4dc4ef473a0d2461a345c74797cf8
Fiqy Ainuzzaqy <fiqy_a@icloud.com>
2019-01-28 20:30:29 +0700

    minor update on seeker's profile

--- resources/views/_seekers/profile.blade.php
+++ resources/views/_seekers/profile.blade.php
@@ -1854,6 +1854,17 @@
                                                                                             <table style="font-size: 14px; margin-top: 0">
                                                                                                 <tr>
                                                                                                     <td>
+                                                                                                        <i class="fa fa-briefcase"></i>
+                                                                                                    </td>
+                                                                                                    <td>&nbsp;Organization
+                                                                                                        Title
+                                                                                                    </td>
+                                                                                                    <td>&nbsp;:&nbsp;
+                                                                                                    </td>
+                                                                                                    <td>{{$row->title}}</td>
+                                                                                                </tr>
+                                                                                                <tr>
+                                                                                                    <td>
                                                                                                         <i class="fa fa-hourglass-start"></i>
                                                                                                     </td>
                                                                                                     <td>&nbsp;Start
